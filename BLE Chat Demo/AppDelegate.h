aeec45c6db82cabc42c755d500a6c02069d7a811
nandev <nanzhao@mit.edu>
2015-12-04 16:12:27 -0500

    Initial Commit

---
+++ BLE Chat Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BLE Chat Demo
+//
+//  Created by Nan Zhao on 12/4/15.
+//  Copyright © 2015 MIT Media Lab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ BLE Chat Demo/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  BLE Chat Demo
+//
+//  Created by Nan Zhao on 12/4/15.
+//  Copyright © 2015 MIT Media Lab. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
